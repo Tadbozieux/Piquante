e10df39666542dcbb03d88d6af3983756ee2dc52
Florentin <florentin93600@gmail.com>
2023-01-27 20:45:30 +0100

    USer controller

--- app.js
+++ app.js
@@ -4,11 +4,21 @@ const mongoose = require('mongoose');
 const Thing = require('./models/thing');
 const stuffRoutes = require('./routes/stuff');
 const userRoutes = require('./routes/user');
+
+
 mongoose.connect('mongodb+srv://tadtad:nicolas9@qsdqsdqsdqsdqsd.iejnsop.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+  });
 
 
 
