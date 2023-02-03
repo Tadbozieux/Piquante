01507916ad3b6a9340dde127a2e4ba3849ac82e6
Florentin <florentin93600@gmail.com>
2023-02-03 11:00:11 +0100

    deblocage sur main Pepper ( erreur de frappe)

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const app = express();
+
 var cors = require('cors')
 const bodyParser = require('body-parser')
-// const jsonParser = bodyParser.json()
-// const urlencodedParser = bodyParser.urlencoded({ extended: false })
-const mongoose = require('mongoose');
-const Thing = require('./models/thing');
+const jsonParser = bodyParser.json()
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+
 const path = require("path");
-const stuffRoutes = require('./routes/stuff');
+
+const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 
-
+const mongoose = require('mongoose');
 
 mongoose.connect('mongodb+srv://tadtad:nicolas9@qsdqsdqsdqsdqsd.iejnsop.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -36,9 +38,10 @@ app.use((req, res, next) => {
   //       autorisation requetes CORS via le  package node.js///////
 
   
+
 app.use(express.json());  
 app.use(bodyParser.json());
-app.use('/api/stuff', stuffRoutes);
+app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
 app.use("/images", express.static(path.join(__dirname, "images")));
 app.use(cors())
